f5263d2ae4f259f6a5f5eb8515806de19ade3e40
Ciaran McGoldrick <cmcgoldrick@awesemo.com>
2019-12-05 16:34:34 -0600

    initial commit

---
+++ foxbet-scrape/Controllers/FoxBet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoxbetScrapeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace FoxbetScrapeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FoxBet : ControllerBase
+    {
+        private readonly ILogger<FoxBet> _logger;
+
+        public FoxBet(ILogger<FoxBet> logger)
+        {
+            var event1 = new Event()
+            {
+                AwayTeam = "Ajax",
+                HomeTeam = "PSV",
+                OfferId = 1,
+                dateTime = DateTime.Now            
+            };
+            var event2 = new Event()
+            {
+                AwayTeam = "Chelsea",
+                HomeTeam = "WestHam",
+                OfferId = 2,
+                dateTime = DateTime.Now
+            };
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+
+        }
+    }
+}
---
+++ foxbet-scrape/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoxbetScrapeAPI.Models
+{
+    public class Event
+    {
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public int OfferId { get; set; }
+        public DateTime dateTime { get; set; }
+    }
+}
